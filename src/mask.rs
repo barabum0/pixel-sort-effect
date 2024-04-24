93549adae51340edc1f9fc16333902e4d6285117
sushka <r.m.poltorabatko@gmail.com>
2024-04-24 16:51:37 +0300

    ✨ feat(mask): добавил параллельную обработку изображений

--- src/mask.rs
+++ src/mask.rs
@@ -1,10 +1,11 @@
 use image::{ImageBuffer, Luma, Rgba};
+use rayon::prelude::*;
 
 fn mask_pixel(v: f64, low_threshold: f64, high_threshold: f64, invert_mask: bool) -> u8 {
     if (low_threshold < v && v < high_threshold) ^ invert_mask { 255 } else { 0 }
 }
 
-pub fn mask_image<F: Fn(&Rgba<u8>) -> f64>(
+pub fn mask_image<F: Fn(&Rgba<u8>) -> f64 + Sync + Send>(
     image: &ImageBuffer::<Rgba<u8>, Vec<u8>>,
     low_threshold: f64,
     high_threshold: f64,
@@ -13,7 +14,15 @@ pub fn mask_image<F: Fn(&Rgba<u8>) -> f64>(
 ) -> ImageBuffer::<Luma<u8>, Vec<u8>> {
     let (width, height) = image.dimensions();
 
-    ImageBuffer::<Luma<u8>, Vec<u8>>::from_fn(width, height, |x, y| {
-        Luma::from([mask_pixel(mask_function(image.get_pixel(x, y)), low_threshold, high_threshold, invert_mask)])
-    })
+    let pixels: Vec<u8> = (0..height).into_par_iter().flat_map(
+        | y | {
+            (0..width).into_iter().map(
+                | x | {
+                    mask_pixel(mask_function(image.get_pixel(x, y)), low_threshold, high_threshold, invert_mask)
+                }
+            ).collect::<Vec<u8>>()
+        }
+     ).collect();
+
+    ImageBuffer::<Luma<u8>, Vec<u8>>::from_vec(width, height, pixels).unwrap()
 }
