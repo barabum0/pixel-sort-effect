8f9f7ec553fe424246197a323988225a7be620b3
sushka <r.m.poltorabatko@gmail.com>
2024-04-24 16:59:59 +0300

    ✨ feat(функции): добавлена перечисляемая Маска с диапазонами

--- src/mask.rs
+++ src/mask.rs
@@ -1,6 +1,32 @@
 use image::{ImageBuffer, Luma, Rgba};
 use rayon::prelude::*;
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum MaskFuncChoice {
+    Luminance,
+    Hue,
+    BrokenHue,
+    Red,
+    Green,
+    Blue,
+    ColorSum
+}
+
+impl MaskFuncChoice {
+    fn get_range(&self) -> (f64, f64) {
+        match self {
+            MaskFuncChoice::Luminance => { (0.0, 255.0) }
+            MaskFuncChoice::Hue => { (0.0, 360.0) }
+            MaskFuncChoice::BrokenHue => { (0.0, 360.0) }
+            MaskFuncChoice::Red => { (0.0, 255.0 ) }
+            MaskFuncChoice::Green => { (0.0, 255.0) }
+            MaskFuncChoice::Blue => { (0.0, 255.0 ) }
+            MaskFuncChoice::ColorSum => { (0.0, 765.0) }
+        }
+    }
+}
+
 fn mask_pixel(v: f64, low_threshold: f64, high_threshold: f64, invert_mask: bool) -> u8 {
     if (low_threshold < v && v < high_threshold) ^ invert_mask { 255 } else { 0 }
 }
@@ -10,19 +36,19 @@ pub fn mask_image<F: Fn(&Rgba<u8>) -> f64 + Sync + Send>(
     low_threshold: f64,
     high_threshold: f64,
     invert_mask: bool,
-    mask_function: F
+    mask_function: F,
 ) -> ImageBuffer::<Luma<u8>, Vec<u8>> {
     let (width, height) = image.dimensions();
 
     let pixels: Vec<u8> = (0..height).into_par_iter().flat_map(
-        | y | {
+        |y| {
             (0..width).into_iter().map(
-                | x | {
+                |x| {
                     mask_pixel(mask_function(image.get_pixel(x, y)), low_threshold, high_threshold, invert_mask)
                 }
             ).collect::<Vec<u8>>()
         }
-     ).collect();
+    ).collect();
 
     ImageBuffer::<Luma<u8>, Vec<u8>>::from_vec(width, height, pixels).unwrap()
 }
